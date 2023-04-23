f7c58b663864f6183bed5eac18005ce75a3c38c1
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-04-23 20:05:15 +0700

    intiial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'grotesk': ['"Hanken Grotesk"', 'sans-serif'],
+      },
+      colors: {
+        lightRed: 'hsl(0, 100%, 67%)',
+        orangeyYellow: 'hsl(39, 100%, 56%)',
+        greenTeal: 'hsl(166, 100%, 37%)',
+        cobaltBlue: 'hsl(234, 85%, 45%)',
+        darkGrayBlue: 'hsl(224, 30%, 27%)',
+        paleBlue: 'hsl(221, 100%, 96%)',
+        lightLavender: 'hsl(241, 100%, 89%)'
+      }
+    },
+  },
+  plugins: [],
+}
+
